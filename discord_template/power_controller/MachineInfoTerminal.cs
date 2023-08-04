4c467b8a6db15a2686202c4c77b6433104024780
DDD <77850088+D-R-D@users.noreply.github.com>
2023-08-04 22:32:38 +0900

    showコマンドを追加

---
+++ discord_template/power_controller/MachineInfoTerminal.cs
@@ -0,0 +1,26 @@
+﻿using discord_template;
+using discord_template.wol;
+using Newtonsoft.Json;
+
+namespace discord_wolbot.power_controller
+{
+    internal class MachineInfoTerminal
+    {
+        public static string GetMachineInfo(string machineName)
+        {
+            string machineInfo = string.Empty;
+
+            MachineInfo machine = Settings.Shared.m_WolMachines.FirstOrDefault(_ => _.Key == machineName).Value;
+            if (machine != null)
+            {
+                machineInfo = JsonConvert.SerializeObject(machine, Formatting.Indented);
+            }
+            else
+            {
+                machineInfo = "unknown machine " + machineName; 
+            }
+
+            return machineInfo;
+        }
+    }
+}
